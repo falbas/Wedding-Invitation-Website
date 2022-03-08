d6f00cf73c95f3d89a4d47236d7c83d85ba437c1
falbas <farhanalbas30@gmail.com>
2022-03-08 19:26:49 +0700

    Merge branch 'develop' of https://github.com/falbas/Wedding-Invitation-Website into develop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,8 +41,8 @@ class Home extends React.Component {
   render() {
     return (
       <div className='home-section'>
-        <div className='d-flex flex-column align-items-center'>
-          <h1>THE WEDDING OF</h1>
+        <div className='d-flex flex-column align-items-center mb-4'>
+          <b className="semiHead h1">THE WEDDING OF</b>
           <div className='w-75 text-center p-md-5'>
             <img src={BrideHome} className='img-fluid' alt='Bride Home' />
           </div>
@@ -77,6 +77,24 @@ class Home extends React.Component {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="mid-section">
+          <div className="d-flex flex-column align-items-center mb-4">
+            <img src="https://wekita.id/wp-content/uploads//2021/07/bismillah.svg" alt="bismillahIcon" />
+            <p className="fontName2">Assalamu'alaikum Wr. Wb.</p>
+            <div className="w-50">
+            <p className="normalFont text-break text-justify text-center">Tanpa mengurangi rasa hormat. Kami mengundang Bapak/Ibu/Saudara/i serta kerabat sekalian untuk menghadiri acara pernikahan kami : </p>
+            </div>
+            <div className="mt-4 mb-4">
+              <img src={BrideHome} className='img-fluid' alt='Bride Home' />
+            </div>
+            <span className='fontName2'>Taki</span>
+            <span className="normalFont2">Putra dari Bapak Taka & ibu Tiki</span>
+            <br />
+            <span className='fontName2'>&</span>
+            <span className='fontName2'>Mitsuha</span>
+            <p className="normalFont2">Putra dari Bapak Mitshu & ibu wa</p>
           </div>
         </div>
       </div>
