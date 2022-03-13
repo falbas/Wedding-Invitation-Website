02528d256a01879f3bff73ba5546e7099dbfa167
falbas <farhanalbas30@gmail.com>
2022-03-13 22:45:14 +0700

    add bootstrap customization

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './App.css'
+import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.render(
